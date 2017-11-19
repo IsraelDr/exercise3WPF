122335cea5d518248f95ebe8bce5faf3c9506276
mosheco110 <mosheco110@gmail.com>
2017-11-19 13:28:23 +0200

    u see it??

--- WPFexercise3/WPFexercise3/MainWindow.xaml.cs
+++ WPFexercise3/WPFexercise3/MainWindow.xaml.cs
@@ -23,44 +23,11 @@ namespace WPFexercise3
     {
         public MainWindow()
         {
-             InitializeComponent();//yfyj
-            }
-            
-             public partial class PrintUserControl1 : UserControl
-             {
-                  public PrintUserControl1()
-                  {
-                      InitializeComponent();
-                      printerCount++;
-                      printerName = "Printer" + printerCount.ToString();
-                      inkCount =100;
-                      PageCount=400;
-                  }
-                  public void randomPrinting()
-                  {
-                      inkCount= inkCount- rand.Next(0,100);
-                      PageCount= PageCount - rand.Next(0, 400);
-                  }
-                  EventHandler<PrinterEventArgs> pageMissing, PageesEmpty;
-                  protected string printerName;
-                  protected decimal inkCount;
-                  protected int PageCount;
-                  static int printerCount=0;
-                  static int     MAX_Pagees=100;
-                  static decimal MIN_ADD_INK = 10;
-                  static decimal MAX_PRINT_INK = 100;
-                  
-                  static int     MAX_PAGES = 400;
-                  static decimal MIN_ADD_PAGE = 10;
-                  static decimal MAX_PRINT_PAGES = 100;
-                  
-                  static Random rand = new Random();
-
-
-
-
-             }
+             InitializeComponent();//u see it??
         }
+            
+           
+        
         
     }
     
